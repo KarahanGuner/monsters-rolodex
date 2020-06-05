cfd012a51947417b6147878f8243595b33c19392
Kara6000 <aqwkarahann@gmail.com>
2020-06-05 15:07:28 +0300

    Updates

---
+++ precache-manifest.75612f2a3239cf15dc1eac3f26942268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f63bdadacf3d16ea1c687f63232d41",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "656a943e79fde285ca1f",
+    "url": "/monsters-rolodex/static/css/main.48773343.chunk.css"
+  },
+  {
+    "revision": "8b8e3e7dea47b9d2130a",
+    "url": "/monsters-rolodex/static/js/2.df5733f9.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/monsters-rolodex/static/js/2.df5733f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "656a943e79fde285ca1f",
+    "url": "/monsters-rolodex/static/js/main.1e7603c6.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
