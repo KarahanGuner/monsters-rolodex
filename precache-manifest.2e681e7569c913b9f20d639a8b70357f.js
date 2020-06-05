7627c4563d06f8e09bfc86cd90512f88d30cddd9
Kara6000 <aqwkarahann@gmail.com>
2020-06-05 14:54:03 +0300

    Updates

---
+++ precache-manifest.2e681e7569c913b9f20d639a8b70357f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c13a8b41a730b3fdd566d0226d0c274",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e53e43669759bec607eb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3103ab1e3a9e55a70d11",
+    "url": "/static/js/2.6c832960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c832960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e53e43669759bec607eb",
+    "url": "/static/js/main.2c8acd11.chunk.js"
+  },
+  {
+    "revision": "321be40e4d0b3008f64a",
+    "url": "/static/js/runtime-main.c34bb27e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
